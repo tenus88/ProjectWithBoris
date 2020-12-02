e2241aea550cacc0af852e3f93e8f41d4024cb77
tenus88 <diachenkoandrei1@gmail.com>
2020-12-02 10:46:17 -0500

    features files

---
+++ src/test/java/com/vytruck/runners/RunnerClass.java
@@ -0,0 +1,29 @@
+package com.vytruck.runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+public class RunnerClass {
+
+
+
+    @RunWith(Cucumber.class)
+    @CucumberOptions(
+            plugin   = {"html:target/cucumber-report.html",
+                    "json:target/cucumber.json",
+                    "rerun:target/rerun.txt"
+            },
+            features = "src/test/resources/features",      //always starts from the 'src'
+            glue     = "com/briteERP/stepDefinitions",   //always starts from the 'com' AND //HOOKS RUNNING BY THE GLUE
+            dryRun   = true,
+            tags     = "@addNote",
+            publish = true
+
+    )
+
+
+    public class CucumberRunner{
+
+    }
+}
